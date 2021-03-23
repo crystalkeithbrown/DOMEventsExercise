ccec2238d34cad0d973989393b65f3d9c27aa666
Crystal Brown <crystalkeithbrown@gmail.com>
2021-03-23 09:45:44 -0400

    DOMEvents Exercise

---
+++ app.js
@@ -0,0 +1,66 @@
+// DOM EVENTS EXERCISE
+
+// 1a
+const button1 = document.querySelector(`#one`);
+// Or
+// const button1 = document.getElementById(`one`);
+
+// 1b
+button1.onclick = () => alert("Cool! You can hear/understand a hummingbird and ants.");
+
+// 2a
+const h3 = document.querySelector(`h3`);
+
+// 2b
+h3.addEventListener(`mouseenter`, () => alert("OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one."));
+// Or
+// h3.addEventListener(`mouseover`, () => alert("OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one."));
+
+// BONUS
+// 3a
+const form = document.querySelector(`form`);
+
+// 3b
+form.addEventListener(`submit`, q => {
+  q.preventDefault();
+  const mess = form.elements.quote.value;
+  alert(mess);
+  // alert(`${mess}`);
+});
+
+// 4a
+const darkMode = document.querySelector(`#dm`);
+
+// 4b
+darkMode.addEventListener(`click`, () => {
+  for (element of document.querySelectorAll(`*`)){
+    element.classList.toggle(`dark-mode`);
+  }
+});
+
+// 5a
+const reality = document.querySelector(`#reality`);
+
+// 5b
+let i = 1;
+function realityJump(){
+  if (i < 3){
+    alert("You have successfully moved to another reality");
+    i++;
+  } else {
+    alert("OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!");
+    reality.removeEventListener(`click`, realityJump);
+  }
+}
+reality.addEventListener(`click`, realityJump);
+// Or
+// let i = 1;
+// reality.addEventListener(`click`, () => {
+//   if (i < 3){
+//     alert("You have successfully moved to another reality");
+//   } else if (i === 3) {
+//     alert("OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!");
+//   }
+//   i++;
+// });
+
